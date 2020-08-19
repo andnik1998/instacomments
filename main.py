e18678137406d0f64e934223cdabde57430c9905
Nikolas Finiotis <2290936f@student.gla.ac.uk>
2020-08-19 12:02:49 +0300

    working code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from time import sleep
-from infofile import *
+from empty_infofile import *
+import numpy
+
 
 class InstaBot:
-
     def __init__(self, username, pw, targetaccount, targetpost, taglist):
-        self.driver = webdriver.Chrome('/usr/bin/chromedriver')
+        self.driver = webdriver.Chrome('C:/chromedriver')
         self.username = username
         self.pw = pw
         self.targetaccount = targetaccount
@@ -18,70 +18,76 @@ class InstaBot:
         # Visits instagram website and logs in with given account
         self.driver.get("https://instagram.com")
         sleep(2)
-        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]")\
-            .click()
-        sleep(2)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]") \
             .send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]") \
             .send_keys(pw)
-        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+        self.driver.find_element_by_xpath('//button[@type="submit"]') \
             .click()
         sleep(4)
 
         # Closes popup asking to add instagram to your homescreen
-        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]") \
             .click()
         sleep(2)
 
-        # Searches for acount given in parameters and clicks of their profile
-        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')\
+        # Closes popup asking to add instagram to your homescreen
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]") \
+            .click()
+        sleep(2)
+
+        # Searches for account given in parameters and clicks of their profile
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input') \
             .send_keys(targetaccount)
         sleep(2)
         clickmessage = "//a[@href='/%s/']" % targetaccount
-        self.driver.find_element_by_xpath(clickmessage)\
+        self.driver.find_element_by_xpath(clickmessage) \
             .click()
         sleep(2)
 
         # Clicks on the post given in the parameters
         clickmessage = "//a[@href='/%s/']" % targetpost
-        self.driver.find_element_by_xpath(clickmessage)\
+        self.driver.find_element_by_xpath(clickmessage) \
             .click()
         sleep(4)
 
         # THREE TAGS PER COMMENT
-        
-        # adds all tags as individual comments on the post
-        iternum = len(taglist)//3 * 3
-        
-        for i in range(0, iternum, 3):
-            tempcomment = ''
-            self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/textarea')\
+
+        for i in range(71, 90):
+            self.driver.find_element_by_xpath(
+                '/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea') \
                 .click()
             sleep(2)
-            tempcomment = taglist[i] + " " + taglist[i+1] + " " + taglist[i+2]
-            self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/textarea')\
+            tempcomment = taglist[i][0] + " " + taglist[i][1]
+            self.driver.find_element_by_xpath(
+                '/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea') \
                 .send_keys(tempcomment)
             sleep(2)
-            self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            self.driver.find_element_by_xpath('//button[@type="submit"]') \
                 .click()
-            sleep(5)
+            sleep(numpy.random.randint(2, 20))
+            print(i)
 
-        # ONE TAG PER COMMENT
+        # for i in taglist:
+        #     iternum = len(i)
+        #     for j in range(0, iternum, len(i)):
+        #         tempcomment = ''
+        #         self.driver.find_element_by_xpath(
+        #             '/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea') \
+        #             .click()
+        #         sleep(2)
+        #         for k in range(len(i)):
+        #             tempcomment += i[k] + " "
+        #         tempcomment = tempcomment[:-1]
+        #         self.driver.find_element_by_xpath(
+        #             '/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea') \
+        #             .send_keys(tempcomment)
+        #         sleep(2)
+        #         self.driver.find_element_by_xpath('//button[@type="submit"]') \
+        #             .click()
+        #         sleep(2)
+        #     sleep(numpy.random.randint(2, 30))
 
-        # adds all tags as individual comments on the post
-        # for tag in taglist:
-        #     self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/textarea')\
-        #         .click()
-        #     sleep(2)
-        #     self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/textarea')\
-        #         .send_keys(tag)
-        #     sleep(2)
-        #     self.driver.find_element_by_xpath('//button[@type="submit"]')\
-        #         .click()
-        #     sleep(4)
-
-        
 
 # initialise instance of bot object
 my_bot = InstaBot(username, pw, accountname, post, taglist)
